0c2603495ab3a17da47ad0346c61057135d11776
muzammaldev-780 <muzammal.murtaza@devsinc.com>
2020-08-26 13:10:50 +0500

    sendgrip updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,20 +25,15 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
+    else
+      render 'new'
     end
   end
-
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
+  
   def update
     respond_to do |format|
       if @user.update(user_params)
